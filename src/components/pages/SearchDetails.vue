aff8e5b2da4e90647f2d84d7688ba2eef1ee7659
Venkatram92 <venkatndd271@gmail.com>
2020-09-13 13:46:29 +0530

    Search

--- src/components/pages/SearchDetails.vue
+++ src/components/pages/SearchDetails.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>Search Details {{ searchText }} {{ shows }}</div>
+  <div class="container">
+    <span v-for="(show, index) in shows" :key="index">
+      <div v-if="show.show.image">
+        <figure class="figure mt-4">
+          <img
+            :src="show.show.image.medium"
+            class="figure-img img-fluid rounded w-75"
+          />
+          <figcaption class="figure-caption text-center">
+            {{ show.show.name }}
+          </figcaption>
+        </figure>
+      </div>
+    </span>
+  </div>
 </template>
 <script>
 import ShowsService from "@/services/ShowsService.js";
 export default {
-  name: "search-details",
   data() {
     return {
-      shows: {}
+      shows: []
     };
   },
   props: {
@@ -28,8 +41,15 @@ export default {
   methods: {
     async getSearchDetails(searchText) {
       let showsResponse = await ShowsService.getSearchShows(searchText);
+      console.log(showsResponse, searchText);
       return showsResponse;
     }
   }
 };
 </script>
+<style scoped>
+.container {
+  display: flex;
+  flex-flow: row wrap;
+}
+</style>
